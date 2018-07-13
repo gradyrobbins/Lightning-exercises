f0fa8a5114c5580ab5ed1698d88894de4e9746e3
Grady Robbins <gradyrobbins13@gmail.com>
2018-07-13 13:08:11 -0500

    added more javascript with lightning exercises and solutions

--- 07-13/main.js
+++ 07-13/main.js
@@ -33,3 +33,47 @@ go("forward", "100");
 
 let element=document.getElementById("sweetspot");
 element.innerHTML+="<p>woop</p>";
+
+
+
+// Given the following set of data about chicken birth rates, you need to filter out the objects where the birth rate value is greater than 15 per 1000 chickens for that year. Store the good years in a new array named `bestYears`.
+const bestYears = [];
+const birthRates = [
+  { year: 1969, birthRate: 13}, 
+  { year: 1970, birthRate: 16}, 
+  { year: 1971, birthRate: 15}, 
+  { year: 1972, birthRate: 11}, 
+  { year: 1973, birthRate: 18}, 
+  { year: 1974, birthRate: 17}, 
+  { year: 1975, birthRate: 9}
+]
+//solution:
+// for (let i = 0; i < birthRates.length; i++) {
+//     const currentYear = birthRates[i];
+//     if (currentYear.birthRate >= 15){ 
+//         bestYears.push(currentYear);
+
+//     }
+// }
+
+// console.log(bestYears);
+
+
+//alternate solution:
+// birthRates.forEach(function (currentYear){
+
+//     if (currentYear.birthRate >= 15){ 
+//                 bestYears.push(currentYear);
+//             }
+// });
+
+// console.log(bestYears);
+
+
+//alternate solution:
+for (currentYear of birthRates) {
+    if (currentYear.birthRate > 15) {
+        bestYears.push(currentYear.year)
+    }
+}
+console.log(bestYears);
